14ba372818b4fd9a41d35415e2240f83bf92547d
CnTeng <rxsnakepi@gmail.com>
2025-06-19 10:38:59 +0800

    chore(nix): add flake-parts and git-hooks-nix

--- flake.nix
+++ flake.nix
@@ -4,34 +4,33 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    git-hooks-nix = {
+      url = "github:cachix/git-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs =
-    inputs@{ self, nixpkgs, ... }:
-    let
-      forAllPkgs =
-        f:
-        nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ] (
-          system: f nixpkgs.legacyPackages.${system}
-        );
-    in
-    {
-      nixosModules.default = import ./nix/module.nix self;
+    inputs@{ self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
 
-      packages = forAllPkgs (pkgs: {
-        ph = pkgs.callPackage ./nix/package.nix { };
-      });
+      imports = [ inputs.git-hooks-nix.flakeModule ];
 
-      checks = forAllPkgs (pkgs: {
-        ph = import ./nix/check.nix self pkgs.nixosTest;
-      });
+      flake.nixosModules.default = import ./nix/module.nix self;
 
-      devShells = forAllPkgs (
-        pkgs:
+      perSystem =
+        { config, pkgs, ... }:
         let
           rust-bin = inputs.rust-overlay.lib.mkRustBin { } pkgs;
           toolchain = rust-bin.selectLatestNightlyWith (
@@ -45,21 +44,31 @@
           );
         in
         {
-          default = pkgs.mkShell {
+          devShells.default = pkgs.mkShell {
             packages = with pkgs; [
               toolchain
               cargo-edit
               cargo-sort
             ];
+            shellHook = config.pre-commit.installationScript;
           };
-        }
-      );
 
-      formatter = forAllPkgs (
-        pkgs:
-        pkgs.nixfmt-tree.override {
-          settings = {
-            formatter.rustfmt = {
+          packages.ph = pkgs.callPackage ./nix/package.nix { };
+
+          checks.ph = pkgs.nixosTest {
+            name = "ph";
+            nodes.machine = {
+              imports = [ self.nixosModules.default ];
+              programs.ph.enable = true;
+            };
+            testScript = ''
+              machine.wait_for_unit("default.target")
+              machine.succeed("which ph")
+            '';
+          };
+
+          formatter = pkgs.nixfmt-tree.override {
+            settings.formatter.rustfmt = {
               command = "rustfmt";
               includes = [ "*.rs" ];
               options = [
@@ -67,10 +76,15 @@
                 "skip_children=true"
               ];
             };
-
-            global.excludes = [ "*.lock" ];
           };
-        }
-      );
+
+          pre-commit.settings.hooks = {
+            treefmt = {
+              enable = true;
+              package = config.formatter;
+            };
+            commitizen.enable = true;
+          };
+        };
     };
 }
