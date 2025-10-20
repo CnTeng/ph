acf7f34fd1c976467245ed3161d020fc9ff8ac83
CnTeng <rxsnakepi@gmail.com>
2025-10-20 23:57:04 +0800

    chore(nix): drop flake-parts and git-hooks-nix

--- flake.nix
+++ flake.nix
@@ -4,87 +4,72 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    flake-parts.url = "github:hercules-ci/flake-parts";
-
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    git-hooks-nix = {
-      url = "github:cachix/git-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs =
-    inputs@{ self, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
+    inputs@{ self, nixpkgs, ... }:
+    let
+      forEachSystem =
+        f:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+        ] (system: f nixpkgs.legacyPackages.${system});
 
-      imports = [ inputs.git-hooks-nix.flakeModule ];
-
-      flake.nixosModules.default = import ./nix/module.nix self;
-
-      perSystem =
-        { config, pkgs, ... }:
+      mkRustToolchain =
+        pkgs:
         let
           rust-bin = inputs.rust-overlay.lib.mkRustBin { } pkgs;
-          toolchain = rust-bin.selectLatestNightlyWith (
-            toolchain:
-            toolchain.default.override {
-              extensions = [
-                "rust-analyzer"
-                "rust-src"
-              ];
-            }
-          );
         in
-        {
-          devShells.default = pkgs.mkShell {
-            packages = with pkgs; [
-              toolchain
-              cargo-edit
-              cargo-sort
-            ];
-            shellHook = config.pre-commit.installationScript;
-          };
+        rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+    in
+    {
+      flake.nixosModules.default = import ./nix/module.nix self;
 
-          packages.ph = pkgs.callPackage ./nix/package.nix { };
-
-          checks.ph = pkgs.nixosTest {
-            name = "ph";
-            nodes.machine = {
-              imports = [ self.nixosModules.default ];
-              programs.ph.enable = true;
-            };
-            testScript = ''
-              machine.wait_for_unit("default.target")
-              machine.succeed("which ph")
-            '';
-          };
-
-          formatter = pkgs.nixfmt-tree.override {
-            settings.formatter.rustfmt = {
-              command = "${toolchain}/bin/rustfmt";
-              includes = [ "*.rs" ];
-              options = [
-                "--config"
-                "skip_children=true"
-              ];
-            };
-          };
-
-          pre-commit.settings.hooks = {
-            treefmt = {
-              enable = true;
-              package = config.formatter;
-            };
-            commitizen.enable = true;
-          };
+      devShells = forEachSystem (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            (mkRustToolchain pkgs)
+            cargo-edit
+            cargo-sort
+          ];
         };
+      });
+
+      packages = forEachSystem (pkgs: {
+        ph = pkgs.callPackage ./nix/package.nix { };
+      });
+
+      checks = forEachSystem (pkgs: {
+        ph = pkgs.nixosTest {
+          name = "ph";
+          nodes.machine = {
+            imports = [ self.nixosModules.default ];
+            programs.ph.enable = true;
+          };
+          testScript = ''
+            machine.wait_for_unit("default.target")
+            machine.succeed("which ph")
+          '';
+        };
+      });
+
+      formatter = forEachSystem (
+        pkgs:
+        (pkgs.nixfmt-tree.override {
+          settings.formatter.rustfmt = {
+            command = "${mkRustToolchain pkgs}/bin/rustfmt";
+            includes = [ "*.rs" ];
+            options = [
+              "--config"
+              "skip_children=true"
+            ];
+          };
+        })
+      );
     };
 }
